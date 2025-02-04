0f6ccc09d0e8ea069d24383e3b1c82865e6d7495
Han <milk88101524@gmail.com>
2025-02-04 13:25:29 +0800

    Initial Commit

---
+++ socket_server_demo/socket_server_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  socket_server_demoApp.swift
+//  socket_server_demo
+//
+//  Created by Han on 2025/2/4.
+//
+
+import SwiftUI
+
+@main
+struct socket_server_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
